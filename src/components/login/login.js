3948f5a29e84ca15533487a7d816cb7a526a6f0b
gabriel <dgabrielfof@gmail.com>
2024-05-16 14:36:35 -0300

    :sparkles: feat: creating a login dialog box

---
+++ src/components/login/login.js
@@ -0,0 +1,15 @@
+export const Login = {};
+
+Login.toggle = () => {
+  const loginBox = document.querySelector(".login_box");
+  const fade = document.querySelector(".login_box_fade");
+  fade.addEventListener("click", () => Login.toggle())
+  const isClosed = loginBox.classList.contains("hide")
+  if (isClosed) {
+    loginBox.classList.remove("hide");
+    fade.classList.remove("hide");
+  } else {
+    loginBox.classList.add("hide");
+    fade.classList.add("hide");
+  }
+}
